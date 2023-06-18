4485ef6028f58b41914d55b9b3af097bef2fbdab
biswajitpanday <biswajitmailid@gmail.com>
2023-06-18 16:03:51 +0000

    Refactor Local Storage.

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,49 +1,60 @@
 import { StorageConstant } from "@/constant";
 
-const keyPrefix = 'BugBuster_';
-
 const storage = {
-    // #region Generic
-    get: (key: string) => {
-        return JSON.parse(window.localStorage.getItem(`${keyPrefix}_${key}`) as string);
-    },
-    set: (key: string, value: string) => {
-        window.localStorage.setItem(`${keyPrefix}_${key}`, JSON.stringify(value));
-    },
-    clear: (key: string) => {
-        window.localStorage.removeItem(`${keyPrefix}_${key}`);
-    },
-    // #endregion
+  // #region Generic
+  get: (key: string) => {
+    return JSON.parse(
+      window.localStorage.getItem(StorageConstant.Prefix() + key) as string
+    );
+  },
+  set: (key: string, value: string) => {
+    window.localStorage.setItem(
+      StorageConstant.Prefix() + key,
+      JSON.stringify(value)
+    );
+  },
+  clear: (key: string) => {
+    window.localStorage.removeItem(StorageConstant.Prefix() + key);
+  },
+  // #endregion
 
-    // #region Token
-    getToken: () => {
-        return JSON.parse(window.localStorage.getItem(StorageConstant.Token()) as string);
-    },
-    setToken: (token: string) => {
-        window.localStorage.setItem(StorageConstant.Token(), JSON.stringify(token));
-    },
-    clearToken: () => {
-        window.localStorage.removeItem(StorageConstant.Token());
-    },
-    // #endregion
+  
+  // #region Token
+  getToken: () => {
+    return JSON.parse(
+      window.localStorage.getItem(StorageConstant.Token()) as string
+    );
+  },
+  setToken: (token: string) => {
+    window.localStorage.setItem(StorageConstant.Token(), JSON.stringify(token));
+  },
+  clearToken: () => {
+    window.localStorage.removeItem(StorageConstant.Token());
+  },
+  // #endregion
 
-    // #region UserProfile
-    getUserProfile: () => {
-        return JSON.parse(window.localStorage.getItem(StorageConstant.UserProfile()) as string)
-    },
-    setUserProfile: (userProfile: string) => {
-        window.localStorage.setItem(StorageConstant.UserProfile(), JSON.stringify(userProfile));
-    },
-    clearUserProfile: () => {
-        window.localStorage.removeItem(StorageConstant.UserProfile());
-    },
-    // #endregion
+  // #region UserProfile
+  getUserProfile: () => {
+    return JSON.parse(
+      window.localStorage.getItem(StorageConstant.UserProfile()) as string
+    );
+  },
+  setUserProfile: (userProfile: string) => {
+    window.localStorage.setItem(
+      StorageConstant.UserProfile(),
+      JSON.stringify(userProfile)
+    );
+  },
+  clearUserProfile: () => {
+    window.localStorage.removeItem(StorageConstant.UserProfile());
+  },
+  // #endregion
 
-    // #region Clear All Local Storage
-    clearStorage:() => {
-        window.localStorage.clear();
-    }
-    // #endregion
-}
+  // #region Clear All Local Storage
+  clearStorage: () => {
+    window.localStorage.clear();
+  },
+  // #endregion
+};
 
 export default storage;
