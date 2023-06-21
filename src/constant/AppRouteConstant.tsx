a1cc3f1cfe614ec4c3b3674dd78b3a436015da8b
biswajitpanday <biswajitmailid@gmail.com>
2023-06-22 02:23:34 +0600

    TopNavBar Route Fix and Some Refactor.

--- src/constant/AppRouteConstant.tsx
+++ src/constant/AppRouteConstant.tsx
@@ -1,13 +1,17 @@
 export const AppRouteConstant = {
-  Root: () => "/app",
-  AuthRoot: () => "/auth/",
-  
+  Root: () => "/",
+
+  AppRoot: () => `${AppRouteConstant.Root()}app`,
+  AuthRoot: () => `${AppRouteConstant.Root()}auth/`,
+
   Login: () => `${AppRouteConstant.AuthRoot()}login`,
   Registration: () => `${AppRouteConstant.AuthRoot()}signup`,
 
-  UserProfile: () => `${AppRouteConstant.Root()}/profile`,
-  Questions: () => `${AppRouteConstant.Root()}/questions`,
-  AskQuestion: () => `${AppRouteConstant.Root()}/ask-question`,
-  QuestionDetail: () => `${AppRouteConstant.Root()}/question-details`,
-  SearchResult: () => `${AppRouteConstant.Root()}/search-result`,
+  UserProfile: () => `${AppRouteConstant.AppRoot()}/profile`,
+  Users: () => `${AppRouteConstant.AppRoot()}/users`,
+  Settings: () => `${AppRouteConstant.AppRoot()}/settings`,
+  Questions: () => `${AppRouteConstant.AppRoot()}/questions`,
+  AskQuestion: () => `${AppRouteConstant.AppRoot()}/ask-question`,
+  QuestionDetail: () => `${AppRouteConstant.AppRoot()}/question-details`,
+  SearchResult: () => `${AppRouteConstant.AppRoot()}/search-result`,
 };
