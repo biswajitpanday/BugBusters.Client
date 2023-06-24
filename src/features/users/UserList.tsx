16092829692c880d789fb8a89dab4be3f8571066
biswajitpanday <biswajitmailid@gmail.com>
2023-06-24 06:06:45 +0600

    Implement Authorization Check by Authorization Component.

--- src/features/users/UserList.tsx
+++ src/features/users/UserList.tsx
@@ -1,15 +1,23 @@
 import { ContentLayout } from "@/components/layout";
+import { Authorization } from "@/lib/Authorization";
+import { Roles } from "@/types";
+import { Forbidden } from "../misc/Forbidden";
 
 export const UserList = () => {
   return (
     <>
-      <ContentLayout title="User List Title Here">
-        <p>User 1</p>
-        <p>User 2</p>
-        <p>User 3</p>
-        <p>User 4</p>
-        <p>User 5</p>
-      </ContentLayout>
+      <Authorization
+        allowedRoles={[Roles.Admin]}
+        forbiddenFallback={<Forbidden />}
+      >
+        <ContentLayout title="User List Title Here">
+          <p>User 1</p>
+          <p>User 2</p>
+          <p>User 3</p>
+          <p>User 4</p>
+          <p>User 5</p>
+        </ContentLayout>
+      </Authorization>
     </>
   );
 };
