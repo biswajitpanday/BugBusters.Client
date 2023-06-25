e30f1eb8d62fbcdcc676df64a51c2afc98ca384c
biswajitpanday <biswajitmailid@gmail.com>
2023-06-25 14:58:38 +0000

    Question Api WIP.

--- src/features/question/AskQuestion.tsx
+++ src/features/question/AskQuestion.tsx
@@ -1,15 +1,8 @@
 import { ContentLayout } from "@/components/layout";
-
-import { TINY_MCE_API_KEY } from "@/config";
-import { Editor } from "@tinymce/tinymce-react";
 import { CkEditor } from "./components/ckEditor/CkEditor";
 
 
 export const AskQuestion = () => {
-  // const handleEditorChange = (content: any, editor: any) => {
-  //   console.log("Content was updated:", content);
-  // };
-
   const editorConfiguration = {
     // toolbar: ["bold", "italic"],
   };
@@ -17,34 +10,7 @@ export const AskQuestion = () => {
   return (
     <>
       <ContentLayout title="Ask Question">
-        
         <CkEditor />
-
-        {/* <Editor
-          apiKey="qagffr3pkuv17a8on1afax661irst1hbr4e6tbv888sz91jc"
-          // initialValue="<p>This is the initial content of the editor</p>"
-          init={{
-            skin: "snow",
-            icons: "thin",
-            placeholder: "Ask a question or post an update...",
-
-            height: 600,
-            menubar: true,
-            plugins: [
-              "advlist autolink lists link image charmap print preview anchor",
-              "searchreplace visualblocks code fullscreen textcolor ",
-              "insertdatetime media table paste code help wordcount",
-            ],
-            textcolor_rows: "4",
-
-            toolbar:
-              "undo redo | styleselect | fontsizeselect| code | bold italic | alignleft aligncenter alignright alignjustify | outdent indent ",
-          }}
-          onEditorChange={handleEditorChange}
-
-          //outputFormat="html"
-          // toolbar="code"
-        /> */}
       </ContentLayout>
     </>
   );
