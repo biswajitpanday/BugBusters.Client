6816055b299331d381e70cc34245ec699410a040
biswajitpanday <biswajitmailid@gmail.com>
2023-07-19 23:06:59 +0000

    Seperate Qustion component and use it on User Details page and question list page.

--- src/features/question/QuestionList.tsx
+++ src/features/question/QuestionList.tsx
@@ -1,15 +1,10 @@
 import { ContentLayout } from "@/components/layout";
 import { useQuestions } from "./api/Question.api";
 import { Spinner } from "@/components/elements/spinner";
-import { Card, Col, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { AppRouteConstant } from "@/constant";
-import { Pluralize } from "@/utils/HelperUtil";
 import { Authorization } from "@/lib/Authorization";
 import { QuestionResponse, Roles } from "@/types";
-import LinesEllipsis from "react-lines-ellipsis";
 import { DataNotFound } from "../misc/DataNotFound";
-import { PostSignature } from "./components/postSignature/PostSignature";
+import { Question } from "./components/Question";
 
 export const QuestionList = () => {
   const questionsQuery = useQuestions();
@@ -21,52 +16,8 @@ export const QuestionList = () => {
     <Authorization allowedRoles={[Roles.User, Roles.Admin]}>
       <ContentLayout title="Question List">
         {questionsQuery.data.map((item: QuestionResponse) => {
-          const {
-            id,
-            title,
-            body,
-            upVoteCount,
-            downVoteCount,
-            answerCount,
-            createdBy,
-            createdAt,
-          } = item;
-
           return (
-            <Card className="mt-1" key={id}>
-              <Row className="pt-3 pb-3">
-                <Col xs={2}>
-                  <div className="text-end">
-                    <div>{Pluralize(upVoteCount, "UpVote")}</div>
-                    <div>{Pluralize(downVoteCount, "DownVote")}</div>
-                    <div>{Pluralize(answerCount, "Answer")}</div>
-                  </div>
-                </Col>
-                <Col xs={10}>
-                  <Link
-                    to={`${AppRouteConstant.Questions()}/${id}`}
-                    className="underline-none"
-                  >
-                    <LinesEllipsis
-                      text={title}
-                      maxLine="2"
-                      ellipsis="..."
-                      trimRight
-                      basedOn="letters"
-                      className="fs-5 fw-bolder"
-                    />
-                  </Link>
-                  {/* <HTMLEllipsis
-                    unsafeHTML={body}
-                    maxLine="3"
-                    ellipsis="..."
-                    basedOn="letters"
-                    className="mt-3"
-                  /> */}
-                  <PostSignature createdBy={createdBy} createdAt={createdAt} className="mt-1" />
-                </Col>
-              </Row>
-            </Card>
+            <Question data={item} />
           );
         })}
       </ContentLayout>
