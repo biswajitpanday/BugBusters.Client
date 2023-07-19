04fcfdc6c5f0a1e331f19f2213b91b941b146553
biswajitpanday <biswajitmailid@gmail.com>
2023-07-19 01:48:09 +0000

    Create PostSignature Component and Refactor Question Details and Question List Page.

---
+++ src/features/question/components/postSignature/PostSignature.tsx
@@ -0,0 +1,41 @@
+import { Badge } from "react-bootstrap";
+import { BbTimeAgo } from "../bbTimeAgo/BbTimeAgo";
+import Avatar from "react-avatar";
+import { GetRandomDarkColor } from "@/utils/HelperUtil";
+import { UserProfile } from "@/types";
+
+type PostSignatureProps = {
+  createdAt: Date;
+  createdBy: UserProfile;
+  showAvatar?: boolean;
+  avatarColor?: string;
+  className?: string;
+};
+
+export const PostSignature = ({
+  createdBy,
+  createdAt,
+  showAvatar = true,
+  avatarColor = GetRandomDarkColor(),
+  className,
+}: PostSignatureProps) => {
+  return (
+    <div className={`float-end me-3 ${className}`}>
+      {showAvatar && (
+        <Avatar
+          name={!!createdBy?.fullName ? createdBy?.email: createdBy?.fullName}
+          size="20"
+          unstyled={false}
+          src=""
+          className="rounded me-1"
+          textSizeRatio={2}
+          color={avatarColor}
+        />
+      )}
+      <Badge bg="primary" className="bg rounded-1 small">
+        {!!createdBy?.fullName ? createdBy?.email: createdBy?.fullName}{" "}
+        <BbTimeAgo title="Asked" dateTime={createdAt} size={12} />
+      </Badge>
+    </div>
+  );
+};
